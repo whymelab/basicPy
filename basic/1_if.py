6df2378b0a79cccb14d2084c0c0ffd2df81809c2
brianmr31 <brianmr31@gmail.com>
2014-05-09 13:23:26 +0700

    Basic Python

---
+++ basic/1_if.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+x = input('Masukan Nilai Integer')
+
+if x < 0 :
+	print 'nilai kurang dari 0',x
+elif x == 0 :
+	print 'nilai sama dengan 0'
+else :
+	print 'nilai lebih dari 0 ',x
+
+
---
+++ basic/2_while.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+x = input('Masukan nilai Integer : ')
+i = 0
+
+while i < x :
+	print 'nilai ke ',i,' dengan x : ',x
+	i+=1
+
